5064255a0a21ba8dde52471d2a83fcf8a71b0841
EggFine <1079742633@qq.com>
2024-06-27 00:39:43 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("是否偶数: Is it even");
+        System.out.println("开发者: EggFine | Development By EggFine");
+        System.out.println("GitHub: https://github.com/EggFine");
+    }
+}
